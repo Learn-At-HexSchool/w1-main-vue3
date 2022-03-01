c63ff028a44454c922c23a69e92a6be8e27e38d1
Aya <90342033+Aya-X@users.noreply.github.com>
2022-03-01 17:10:05 +0800

    Update the package for prettier
    Set up for VSCode.

--- vue.config.js
+++
@@ -1,9 +0,0 @@
-// Vue-UI 的「設定」->「Vue Cli」->「公開路徑」
-// vue.config.js file to be placed in the root of your repository
-// https://cli.vuejs.org/guide/deployment.html#github-pages
-
-module.exports = {
-  // 預設狀況下，部署到 GitHub Pages 可能會無法顯示畫面
-  // 因此需要更改路徑
-  publicPath: './',
-};
